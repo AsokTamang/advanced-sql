6b0e8823b0273aa49c878e2cf106e877c5b94fa3
AsokTamang <ashoktmg2058@gmail.com>
2026-01-14 15:00:42 -0500

    most profitable movies

--- practice.sql
+++ practice.sql
@@ -1,5 +1,4 @@
---Revenue Analysis by Industry
-Find the total revenue and average budget for each industry, but only include movies released after 2010. Convert all currencies to a common unit for fair comparison.--
+--Revenue Analysis by Industry. Find the total revenue and average budget for each industry, but only include movies released after 2010. Convert all currencies to a common unit for fair comparison.--
 SELECT SUM(
  IF(f.currency="INR", f.revenue*0.01, f.revenue)
  )AS total_revenue , AVG(IF(f.currency = "INR",f.budget * 0.01,f.budget)) as average_budget, m.industry FROM financials as f LEFT JOIN movies as m
@@ -7,10 +6,19 @@ ON m.movie_id = f.movie_id
 WHERE m.release_year>2010
 GROUP BY m.industry;
 
---  Actor Collaboration Network
-Find all pairs of actors who have worked together in at least 2 movies. Show their names and the count of collaborations. --
+--  Actor Collaboration Network. Find all pairs of actors who have worked together in at least 2 movies. Show their names and the count of collaborations. --
 SELECT a.name, COUNT(*) from actors as a
 JOIN movie_actor as ma ON a.actor_id = ma.actor_id
 JOIN movies as m ON m.movie_id = ma.movie_id
 GROUP BY a.name
 HAVING COUNT(*) > 2;
+
+
+
+-- Most Profitable Movies. Calculate profit (revenue - budget) for each movie, accounting for different units (Millions, Billions, etc.). Rank the top 10 most profitable movies.--
+SELECT SUM(IF(f.currency="INR", f.revenue*0.01, f.revenue)-IF(f.currency="INR", f.budget*0.01, f.budget)) as profit,m.title FROM financials as f JOIN movies as m
+ON f.movie_id = m.movie_id
+GROUP BY m.title
+ORDER BY profit DESC
+LIMIT 0,10;
+
